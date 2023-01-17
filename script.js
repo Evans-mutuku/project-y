5007791fec40e3e05d551ebfc9a1060cb10fcc57
Evans-mutuku <evansnyamai98@gmail.com>
2023-01-17 12:02:06 +0300

    done with Navigation Bar

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBEC8nZF1gLS8mEJiNfpxyiLfC79JqXkA0",
-    authDomain: "task-man-c7972.firebaseapp.com",
-    projectId: "task-man-c7972",
-    storageBucket: "task-man-c7972.appspot.com",
-    messagingSenderId: "1047263220371",
-    appId: "1:1047263220371:web:1b0b88233ba16509e4b366",
-    measurementId: "G-9VR0K26NHG"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBEC8nZF1gLS8mEJiNfpxyiLfC79JqXkA0",
+//     authDomain: "task-man-c7972.firebaseapp.com",
+//     projectId: "task-man-c7972",
+//     storageBucket: "task-man-c7972.appspot.com",
+//     messagingSenderId: "1047263220371",
+//     appId: "1:1047263220371:web:1b0b88233ba16509e4b366",
+//     measurementId: "G-9VR0K26NHG"
+// };
 
-firebaseConfig.initializeApp(firebaseConfig)
+// firebaseConfig.initializeApp(firebaseConfig)
 
-// Listening to load event on window
-// Reading the number of tasks stored in the database
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
