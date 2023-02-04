37de9e1a7a64d468047fb79f144efcbb9e5e4609
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-04 11:45:32 +0300

    preparing add task code

--- createTask.js
+++ createTask.js
@@ -9,3 +9,23 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+// function invoked by add btn
+// function to store the task entered by the user in the database
+// read and clear the fields->store the task in DB with a unique code->update the total tasks in DB->
+// show the task under taskbar in form of a card
+
+function storeTask(event) {
+  event.preventDefault();
+  const task = document.getElementById("task").value;
+  const desc = document.getElementById("desc").value;
+  const taskTime = document.getElementById("time").value;
+}
+
+// function invoked by show all btn
+// if no tasks available then display "No pending tasks"
+// Remove any tasks if displayed inside the task bar
+// read the data from the DB and display in form of cards
+// adjust the color and disabled attribute of the buttons based on whether the task is completed or not
+
+function showAll() {}
